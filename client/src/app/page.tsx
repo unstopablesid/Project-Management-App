e419a11e04f21b17b44f914a1460d9886e8ec6a4
Mayur Nikumbh <mayurnikumbh2004@gmail.com>
2025-04-25 23:53:27 +0530

    feat: initialize project with Next.js, Tailwind CSS, and global styles
    - Added global CSS styles with dark mode support.
    - Created layout component to manage page structure and fonts.
    - Implemented a simple home page displaying "Hello World".
    - Configured TypeScript settings for the project.
    - Set up package.json with project metadata and basic scripts.

---
+++ client/src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="h-screen flex justify-center items-center">
+      <div className="text-5xl font-sans">Hello World</div>
+    </div>
+  );
+}
