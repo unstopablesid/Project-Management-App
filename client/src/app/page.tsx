11692915505c3af5be6ca690f7beebfd5662d243
Mayur Nikumbh <mayurnikumbh2004@gmail.com>
2025-04-30 23:01:40 +0530

    feat: update dependencies and improve styling
    - Added postcss as a dependency in package.json and package-lock.json.
    - Updated @tailwindcss/postcss and tailwindcss versions to 4.1.5 in package.json and package-lock.json.
    - Removed unused SVG files from the public directory.
    - Changed background color in globals.css from gray-50 to black.
    - Updated text color to white for the "Hello World" text in page.tsx.
    - Added tailwind.config.js for Tailwind CSS configuration.
    - Added prettier and prettier-plugin-tailwindcss as devDependencies in the root package.json and updated package-lock.json.

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="h-screen flex justify-center items-center">
-      <div className="text-5xl font-sans">Hello World</div>
+      <div className="text-5xl text-white font-sans">Hello World</div>
     </div>
   );
 }
