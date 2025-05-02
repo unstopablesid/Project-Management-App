6ab7ce6252615c142a1d345f6c7a2e8b776e3bf7
Mayur Nikumbh <mayurnikumbh2004@gmail.com>
2025-05-02 22:25:51 +0530

    fix: correct Navbar alignment and update text color in DashbordWrapper

--- client/src/app/(components)/Navbar/index.tsx
+++ client/src/app/(components)/Navbar/index.tsx
@@ -3,9 +3,10 @@ import {Search } from 'lucide-react';
 
 const Navbar = () => {
   return (
-    <div className='flex items-center justify-between bg-white px-4 py3 dark:bg-black dark:px-4 dark:py-3 '>
+    <div className='flex item-center justify-center bg-white px-4 py3 dark:bg-black dark:px-4 dark:py-3 '>
       <div className='relative flex h-min w-[200px]'>
         <Search className='absolute left left-[4px] top-1/2 mr-2 h-5 w-5 -translate-y-1/2 transform cursor-pointer dark:text-white'/>
+        <input type="search" />
       </div>
      </div>
   )
--- client/src/app/dashbordWrapper.tsx
+++ client/src/app/dashbordWrapper.tsx
@@ -4,13 +4,13 @@ import Navbar from "@/app/(components)/Navbar/index";
 
 const DashbordWrapper = ({children}: {children: React.ReactNode}) => {     
 
-    return ( <div className='flex min-h-screen w-full bg-grey-50 text-grey-900'>
+    return ( <div className='flex min-h-screen w-full bg-grey-50 text-white'>
 
         {/* Sidebar */}
         sidebar
 
-        <main className={`flex w-full felx-col bg-grey-50 dark:bg-dark-bg md:pl-64`}>
            <Navbar />
+        <main className={`flex w-full felx-col bg-grey-50 dark:bg-dark-bg md:pl-64`}>
             {children}
         </main>
     </div>
